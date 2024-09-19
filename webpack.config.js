83d84a615a7e02571169482d31e611dc34fcffbc
Pranshu-15 <pranshu923@gmail.com>
2024-09-19 17:29:01 +0530

    completed

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin= require('html-webpack-plugin');
 module.exports = {
-    entry: './src/index.js',
+    // entry: './src/index.js',
+    entry: ["babel-polyfill", "./src/index.js"],
 
     output: {
         path: path.join(__dirname,"/dist"),
